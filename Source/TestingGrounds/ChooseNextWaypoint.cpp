f307653584beb5fa798df5b731fc1c6f6911544e
Tamás Gyetvai <34650749+gyetvaitamas@users.noreply.github.com>
2023-10-10 18:59:07 +0200

    Finally switching from blueprint to C++

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,27 +1,33 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Made in the The Unreal Engine Developer Course - Learn C++ & Make Games course by Gyetvai Tamás
 
 
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	// TODO: protect against empty
-	// 
+	// Get the patrol route component
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) 
+	{ 
+		return EBTNodeResult::Failed;
+	}
+
 	// Get the patrol points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points!"));
+		return EBTNodeResult::Failed;
+	}
 
 	// Set next waypoint
 	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 	BlackBoardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
-
-	//UE_LOG(LogTemp, Warning, TEXT("AI in C++! Waypoint index: %i"), Index);
 
 	// Cycle the index
 	auto NextIndex = (Index + 1) % PatrolPoints.Num();
--- Source/TestingGrounds/PatrolRoute.cpp
+++ Source/TestingGrounds/PatrolRoute.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Made in the The Unreal Engine Developer Course - Learn C++ & Make Games course by Gyetvai Tamás
 
 
 #include "PatrolRoute.h"
