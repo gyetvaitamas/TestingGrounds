d0170348c6c11503c486fb141f0d5984a68434f6
Tamás Gyetvai <34650749+gyetvaitamas@users.noreply.github.com>
2023-10-10 16:21:44 +0200

    Getting waypoint index

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,10 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "BehaviorTree/BlackboardComponent.h"
 #include "ChooseNextWaypoint.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++"));
+	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
+
+	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	UE_LOG(LogTemp, Warning, TEXT("AI in C++! Waypoint index: %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
